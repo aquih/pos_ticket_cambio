4a9a4e608f015e94dcb087c8c36945963b50d2a3
Rodrigo Fernández <rodrigo@aquih.com>
2025-07-31 23:10:52 +0000

    Siempre imprimir al menos un ticket de cambio.

--- static/src/overrides/components/control_buttons/pos_ticket_cambio_button.js
+++ static/src/overrides/components/control_buttons/pos_ticket_cambio_button.js
@@ -3,13 +3,12 @@
 import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
 import { NumberPopup } from "@point_of_sale/app/utils/input_popups/number_popup";
 import { patch } from "@web/core/utils/patch";
-import { _t } from "@web/core/l10n/translation";
 
 patch(ControlButtons.prototype, {
     async clickTagNumber() {
         this.dialog.add(NumberPopup, {
-            title: _t("Cantidad de Tickets"),
-            placeholder: _t("0"),
+            title: "Cantidad de Tickets",
+            placeholder: "1",
             getPayload: async (cantidad) => {
                 const order = this.pos.get_order();
                 order.cantidad_tickets_cambio = cantidad;
--- static/src/overrides/models/por_order.js
+++ static/src/overrides/models/por_order.js
@@ -6,6 +6,6 @@ import { PosOrder } from "@point_of_sale/app/models/pos_order";
 patch(PosOrder.prototype, {
     setup(vals) {
         super.setup(vals);
-        this.cantidad_tickets_cambio = vals.cantidad_tickets_cambio || 0;
+        this.cantidad_tickets_cambio = vals.cantidad_tickets_cambio || (this.config.ticket_cambio ? 1 : 0);
     },
 });
